11316318016b2920e1e7c4006086b80b3c6b3fec
WilliAndHan <william.a.hanantha@gmail.com>
2026-06-15 07:49:21 +0700

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -109,5 +109,5 @@ Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
 Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
 Route::post('/siswa/store', [SiswaController::class, 'store'])->name('siswa.store');
 Route::get('/siswa/edit/{nrp}', [SiswaController::class, 'edit'])->name('siswa.edit');
-Route::post('/siswa/update/{nrp}', [SiswaController::class, 'update'])->name('siswa.update');
-Route::get('/siswa/destroy/{nrp}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
+Route::put('/siswa/update/{nrp}', [SiswaController::class, 'update'])->name('siswa.update');
+Route::delete('/siswa/destroy/{nrp}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
